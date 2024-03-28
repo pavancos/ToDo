368f76d1b402d914332729893708f7286a364019
tej-mahender <148678239+tej-mahender@users.noreply.github.com>
2024-03-28 16:54:14 +0530

    Update ToDoList.cpp (#3)

--- ToDoList.cpp
+++ ToDoList.cpp
@@ -10,9 +10,8 @@ using namespace std;
 
 int ID = 0;
 
-void setTime(struct tm &I)
-{
-    cout << "Enter deadline date (DDMMYYYY): \n";
+void setTime(struct tm &I){
+    cout << "\tEnter deadline date (DDMMYYYY): ";
     string d;
     cin >> d;
     char arr[d.length() + 1];
@@ -25,23 +24,20 @@ void setTime(struct tm &I)
     I.tm_year = (int(arr[4] - '0') * 1000) + (int(arr[5] - '0') * 100) + (int(arr[6] - '0') * 10) + (int(arr[7] - '0')) - 1900;
 }
 
-struct todo
-{
+struct todo{
     int id;
     string task;
     struct tm dedLine;
     int remDays;
 
-    bool operator<(const todo &other) const
-    {
+    bool operator<(const todo &other) const{
         return remDays > other.remDays;
     }
 };
 
 priority_queue<todo> tasks;
 
-void addtodo()
-{
+void addtodo(){
     cout << "\n\tEnter new task: ";
     string task;
     cin.ignore();
@@ -65,149 +61,93 @@ void addtodo()
     cout << "\n\tTask has been added successfully";
 }
 
-void print(const todo &task)
-{
-    cout << "\nTask ID: " << task.id << endl;
-    cout << "Task: " << task.task << endl;
-    cout << "Deadline: " << task.dedLine.tm_mday << "/" << task.dedLine.tm_mon + 1 << "/" << task.dedLine.tm_year + 1900 << endl;
-    cout << "Remaining Days: " << task.remDays << endl;
+void print(const todo &task){
+    cout << "\n\tTask ID: " << task.id << endl;
+    cout << "\tTask: " << task.task << endl;
+    cout << "\tDeadline: " << task.dedLine.tm_mday << "/" << task.dedLine.tm_mon + 1 << "/" << task.dedLine.tm_year + 1900 << endl;
+    cout << "\tRemaining Days: " << task.remDays << endl;
 }
 
-void readData()
-{
-    if (tasks.empty())
-    {
-        cout << "\nNo tasks available.\n";
+void readData(){
+    if (tasks.empty()){
+        cout << "\n\tNo tasks available.\n";
         return;
     }
 
     priority_queue<todo> temp = tasks;
-    cout << "\nTasks List:\n";
-    while (!temp.empty())
-    {
+    cout << "\n\tTasks List:\n";
+    while (!temp.empty()){
         print(temp.top());
         temp.pop();
     }
 }
 
-int searchData()
-{
-    if (tasks.empty())
-    {
-        cout << "\nNo tasks available.\n";
+int searchData(){
+    if (tasks.empty()){
+        cout << "\n\tNo tasks available.\n";
         return -1;
     }
 
     int NoOfDays;
-    cout << "\nEnter the number of days: ";
+    cout << "\n\tEnter the number of days: ";
     cin >> NoOfDays;
 
     priority_queue<todo> temp = tasks;
     bool found = false;
-    while (!temp.empty())
-    {
-        if (temp.top().remDays < NoOfDays)
-        {
+    while (!temp.empty()){
+        if (temp.top().remDays <= NoOfDays){
             print(temp.top());
             found = true;
         }
         temp.pop();
     }
 
-    if (!found)
-    {
-        cout << "\nNo tasks with remaining days less than " << NoOfDays << " found.\n";
+    if (!found){
+        cout << "\n\tNo tasks with remaining days less than " << NoOfDays << " found.\n";
     }
     return -1;
 }
 
-void deleteData()
-{
-    if (tasks.empty())
-    {
-        cout << "\nNo tasks available.\n";
+void deleteData(){
+    if (tasks.empty()){
+        cout << "\n\tNo tasks available.\n";
         return;
     }
 
     int id;
-    cout << "\nEnter task ID to delete: ";
+    cout << "\n\tEnter task ID to delete: ";
     cin >> id;
 
     priority_queue<todo> temp;
     bool found = false;
-    while (!tasks.empty())
-    {
-        if (tasks.top().id == id)
-        {
+    while (!tasks.empty()){
+        if (tasks.top().id == id){
             found = true;
-            cout << "\nDeleting Task " << id << ":\n";
+            cout << "\n\tDeleting Task " << id << ":\n";
             tasks.pop(); // Discard the task to delete
         }
-        else
-        {
+        else{
             temp.push(tasks.top());
+            tasks.pop();
         }
-        tasks.pop();
     }
-
     tasks = temp;
-    if (!found)
-    {
-        cout << "\nTask with ID " << id << " not found.\n";
+    if (!found){
+        cout << "\n\tTask with ID " << id << " not found.\n";
     }
 }
 
-void updateData()
-{
-    if (tasks.empty())
-    {
-        cout << "\nNo tasks available.\n";
-        return;
-    }
-
-    int id;
-    cout << "\nEnter task ID to update: ";
-    cin >> id;
-
-    priority_queue<todo> temp;
-    bool found = false;
-    while (!tasks.empty())
-    {
-        if (tasks.top().id == id)
-        {
-            found = true;
-            cout << "\nUpdating Task " << id << ":\n";
-            addtodo();
-        }
-        else
-        {
-            temp.push(tasks.top());
-        }
-        tasks.pop();
-    }
-
-    tasks = temp;
-    if (!found)
-    {
-        cout << "\nTask with ID " << id << " not found.\n";
-    }
-}
-
-int main()
-{
-    while (true)
-    {
+int main(){
+    while (true){
         cout << "\n\n\t1. Add Task";
         cout << "\n\t2. Display Tasks";
         cout << "\n\t3. Search Task by Remaining Days";
         cout << "\n\t4. Delete Task";
-        cout << "\n\t5. Update Task";
-        cout << "\n\t6. Exit";
+        cout << "\n\t5. Exit";
         int choice;
         cout << "\n\n\tEnter choice: ";
         cin >> choice;
-        switch (choice)
-        {
+        switch (choice){
         case 1:
             addtodo();
             break;
@@ -221,9 +161,6 @@ int main()
             deleteData();
             break;
         case 5:
-            updateData();
-            break;
-        case 6:
             cout << "\n\tExiting program...";
             return 0;
         default:
@@ -231,3 +168,4 @@ int main()
         }
     }
 }
+
